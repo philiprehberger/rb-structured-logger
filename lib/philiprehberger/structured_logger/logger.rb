75f98775e0c7b15d30d21b804cea7826a0861ae2
Reverend Philip <me@philiprehberger.com>
2014-12-02 08:48:31 -0600

    Add level getter, with_context, silence, and log_exception methods
    - level returns the current log level as a symbol
    - with_context temporarily merges context for a block
    - silence temporarily raises log level to suppress noise
    - log_exception logs exception class, message, and backtrace

--- lib/philiprehberger/structured_logger/logger.rb
+++ lib/philiprehberger/structured_logger/logger.rb
@@ -8,7 +8,7 @@ module Philiprehberger
     class Logger
       LEVELS = { debug: 0, info: 1, warn: 2, error: 3, fatal: 4 }.freeze
 
-      attr_reader :context
+      attr_reader :context, :level
 
       # @param output [IO] writable output (default: $stdout)
       # @param level [Symbol] minimum log level
@@ -35,6 +35,46 @@ module Philiprehberger
       # @return [Logger]
       def child(**extra)
         self.class.new(output: @output, level: @level, context: @context.merge(extra))
+      end
+
+      # Temporarily merge extra context for the duration of a block.
+      #
+      # @param extra [Hash] additional context
+      # @yield block during which the extra context is active
+      def with_context(**extra, &block)
+        @monitor.synchronize do
+          original = @context
+          @context = @context.merge(extra).freeze
+          block.call
+        ensure
+          @context = original
+        end
+      end
+
+      # Temporarily raise the log level for the duration of a block.
+      #
+      # @param temp_level [Symbol] level to use during the block
+      # @yield block during which the level is raised
+      def silence(temp_level = :fatal, &block)
+        @monitor.synchronize do
+          original = @level
+          @level = temp_level
+          block.call
+        ensure
+          @level = original
+        end
+      end
+
+      # Log an exception with its class, message, and backtrace.
+      #
+      # @param exception [Exception] the exception to log
+      # @param level [Symbol] log level to use
+      # @param extra [Hash] additional context
+      def log_exception(exception, level: :error, **extra)
+        log(level, exception.message, **extra.merge(
+          error_class: exception.class.name,
+          backtrace: exception.backtrace || []
+        ))
       end
 
       LEVELS.each_key do |lvl|
